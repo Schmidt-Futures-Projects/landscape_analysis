06eea8a5a301111aba1f285ede384a484e4087fc
Liam Considine <lconsidine@schmidtentitiesprojects.org>
2024-07-26 19:41:25 -0400

    Step before adding a component library

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
+# Notes to Start Developing
+`npm run dev`
+`npm run lint`
+
 # React + TypeScript + Vite
 
 This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
-
+  
   return (
     <>
       <div>
@@ -21,7 +21,9 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
+        <div></div>
         <p>
+
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
